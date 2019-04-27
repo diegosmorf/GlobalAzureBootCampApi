24f1e3c0a8571afed9c2c29750b60772a3d57668
Diego Cardoso <dcardoso@harbourvest.com>
2019-04-27 14:06:10 -0300

    stars wars

---
+++ GlobalAzureBootCampApi/Controllers/StarsWarsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GlobalAzureBootCampApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StarWarsController : ControllerBase
+    {
+        private readonly string[] repository = new string[] { "Luke Skywalker",
+                "C-3PO",
+                "R2-D2",
+                "Darth Vader",
+                "Leia Organa",
+                "Han Solo",
+                "Obi-Wan Kenobi",
+                "Yoda",
+                "Rey",
+                "Kylo Ren",
+                "BB-8"
+            };
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return repository;
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public ActionResult<IEnumerable<string>> Get(string name)
+        {
+            return Ok(repository.Where(x => x.Contains(name)));
+        }
+    }
+}
